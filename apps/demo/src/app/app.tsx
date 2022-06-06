f353b2e6b240c9956284f4109c87f23b39a4faf7
Aaron R Miller <aaron@stackbit.com>
2022-06-06 12:38:42 -0700

    clean up scrolling

--- apps/demo/src/app/app.tsx
+++ apps/demo/src/app/app.tsx
@@ -1,16 +1,78 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { RichTextEditor } from '@stackbit/rich-text-editor';
-import styled from 'styled-components';
+import { CssBaseline, Link, Typography } from '@mui/material';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+import { theme } from './theme';
 
-export function App() {
+function Copyright() {
   return (
-    <StyledApp>
-      <RichTextEditor />
-    </StyledApp>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      sx={{ padding: 1 }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://stackbit.com/">
+        Stackbit, Inc.
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+// export type SbEditorState = PlatesStoreState<Value>;
+
+// const StackbitEditor: ForwardRefRenderFunction<
+//   StackbitEditorRef,
+//   StackbitEditorProps
+// > = ({ classes, ...props }, ref) => {
+//   // const [state, setState] = useState<PlatesStoreState>({});
+//   const [focus, setFocus] = useState(true);
+//
+//   return (
+//     <div className={classes.root}>
+//       <Container component="main" maxWidth="lg" className={classes.container}>
+//         <Paper variant="outlined" className={classes.editorContainer}>
+//           <Slide appear={false} direction="down" in={focus}>
+//             <AppBar
+//               position="absolute"
+//               color="default"
+//               elevation={0}
+//               sx={{
+//                 position: 'relative',
+//                 borderBottom: (t) => `1px solid ${t.palette.divider}`,
+//               }}
+//             >
+//               <Toolbar>
+//                 <Typography variant="h6" color="inherit" noWrap>
+//                   Stackbit Rich Text Editor Demo
+//                 </Typography>
+//               </Toolbar>
+//             </AppBar>
+//           </Slide>
+//           {/*<Toolbar />*/}
+//           <Box className={classes.editor}>
+//             <RichTextEditor id="main" />
+//           </Box>
+//         </Paper>
+//         <Copyright />
+//       </Container>
+//     </div>
+//   );
+// };
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RichTextEditor />
+      <Copyright />
+    </ThemeProvider>
+  );
+}
+
+// export default withStyles(styles, { withTheme: true, name: 'StackbitEditor' })(
+//   forwardRef(RichTextEditor)
+// );
--- libs/rich-text-editor/src/rich-text-editor.tsx
+++ libs/rich-text-editor/src/rich-text-editor.tsx
@@ -1,27 +1,42 @@
 import 'tippy.js/dist/tippy.css';
 // import './index.css';
-
 import React from 'react';
 import {
-  createPlateUI,
-  HeadingToolbar,
-  Plate,
+  ColorPickerToolbarDropdown,
   createAlignPlugin,
   createAutoformatPlugin,
   createBlockquotePlugin,
   createBoldPlugin,
+  createCodeBlockPlugin,
   createCodePlugin,
+  createComboboxPlugin,
+  createDeserializeCsvPlugin,
+  createDeserializeDocxPlugin,
+  createDeserializeMdPlugin,
   createExitBreakPlugin,
+  createFontBackgroundColorPlugin,
+  createFontColorPlugin,
+  createFontFamilyPlugin,
+  createFontSizePlugin,
+  createFontWeightPlugin,
   createHeadingPlugin,
   createHighlightPlugin,
-  createKbdPlugin,
+  createHorizontalRulePlugin,
   createImagePlugin,
+  createIndentPlugin,
   createItalicPlugin,
+  createJuicePlugin,
+  createKbdPlugin,
+  createLineHeightPlugin,
   createLinkPlugin,
   createListPlugin,
   createMediaEmbedPlugin,
+  createMentionPlugin,
   createNodeIdPlugin,
+  createNormalizeTypesPlugin,
   createParagraphPlugin,
+  createPlateUI,
+  createPlugins,
   createResetNodePlugin,
   createSelectOnBackspacePlugin,
   createSoftBreakPlugin,
@@ -32,43 +47,28 @@ import {
   createTodoListPlugin,
   createTrailingBlockPlugin,
   createUnderlinePlugin,
-  createComboboxPlugin,
-  createMentionPlugin,
-  createIndentPlugin,
-  createFontColorPlugin,
-  createFontBackgroundColorPlugin,
-  createDeserializeMdPlugin,
-  createDeserializeCsvPlugin,
-  createNormalizeTypesPlugin,
-  createFontSizePlugin,
-  createHorizontalRulePlugin,
-  createPlugins,
-  createDeserializeDocxPlugin,
-  createJuicePlugin,
-  createLineHeightPlugin,
-  createCodeBlockPlugin,
-  createFontFamilyPlugin,
-  createFontWeightPlugin,
-  ColorPickerToolbarDropdown,
-  MARK_COLOR,
-  MARK_BG_COLOR,
+  HeadingToolbar,
+  ImageToolbarButton,
   LineHeightToolbarDropdown,
   LinkToolbarButton,
-  ImageToolbarButton,
+  MARK_BG_COLOR,
+  MARK_COLOR,
   MediaEmbedToolbarButton,
   MentionCombobox,
+  Plate,
+  PlateProps,
 } from '@udecode/plate';
 import { withStyledPlaceHolders } from './config/components/withStyledPlaceHolders';
 import { CONFIG } from './config/config';
 import { VALUES } from './config/values/values';
 import {
-  BasicElementToolbarButtons,
-  ListToolbarButtons,
-  IndentToolbarButtons,
-  BasicMarkToolbarButtons,
   AlignToolbarButtons,
-  TableToolbarButtons,
+  BasicElementToolbarButtons,
+  BasicMarkToolbarButtons,
+  IndentToolbarButtons,
+  ListToolbarButtons,
   MarkBallonToolbar,
+  TableToolbarButtons,
 } from './config/components/Toolbars';
 import { FormatColorText } from '@styled-icons/material/FormatColorText';
 import { Check } from '@styled-icons/material/Check';
@@ -77,8 +77,66 @@ import { Image } from '@styled-icons/material/Image';
 import { LineWeight } from '@styled-icons/material/LineWeight';
 import { Link } from '@styled-icons/material/Link';
 import { OndemandVideo } from '@styled-icons/material/OndemandVideo';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Zoom from '@mui/material/Zoom';
 
-const RichTextEditor = () => {
+export type IRichTextEditorProps = PlateProps;
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  // window?: () => Window;
+  children: React.ReactElement;
+}
+
+function ScrollTop(props: Props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    // target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+}
+
+export default function RichTextEditor(
+  props: React.HtmlHTMLAttributes<HTMLDivElement>
+) {
+  console.log('RichTextEditor(props)', props);
   let components = createPlateUI();
   components = withStyledPlaceHolders(components);
 
@@ -133,42 +191,51 @@ const RichTextEditor = () => {
   );
 
   return (
-    <Plate
-      id="playground"
-      editableProps={CONFIG.editableProps}
-      initialValue={VALUES['playground']}
-      plugins={plugins}
-    >
-      <HeadingToolbar>
-        <BasicElementToolbarButtons />
-        <ListToolbarButtons />
-        <IndentToolbarButtons />
-        <BasicMarkToolbarButtons />
-        <ColorPickerToolbarDropdown
-          pluginKey={MARK_COLOR}
-          icon={<FormatColorText />}
-          selectedIcon={<Check />}
-          tooltip={{ content: 'Text color' }}
-        />
-        <ColorPickerToolbarDropdown
-          pluginKey={MARK_BG_COLOR}
-          icon={<FontDownload />}
-          selectedIcon={<Check />}
-          tooltip={{ content: 'Highlight color' }}
-        />
-        <AlignToolbarButtons />
-        <LineHeightToolbarDropdown icon={<LineWeight />} />
-        <LinkToolbarButton icon={<Link />} />
-        <ImageToolbarButton icon={<Image />} />
-        <MediaEmbedToolbarButton icon={<OndemandVideo />} />
-        <TableToolbarButtons />
-      </HeadingToolbar>
+    <div {...props}>
+      <Plate
+        id="playground"
+        editableProps={CONFIG.editableProps}
+        initialValue={VALUES['playground']}
+        plugins={plugins}
+      >
+        <AppBar color="inherit">
+          <Toolbar>
+            <HeadingToolbar>
+              <BasicElementToolbarButtons />
+              <ListToolbarButtons />
+              <IndentToolbarButtons />
+              <BasicMarkToolbarButtons />
+              <ColorPickerToolbarDropdown
+                pluginKey={MARK_COLOR}
+                icon={<FormatColorText />}
+                selectedIcon={<Check />}
+                tooltip={{ content: 'Text color' }}
+              />
+              <ColorPickerToolbarDropdown
+                pluginKey={MARK_BG_COLOR}
+                icon={<FontDownload />}
+                selectedIcon={<Check />}
+                tooltip={{ content: 'Highlight color' }}
+              />
+              <AlignToolbarButtons />
+              <LineHeightToolbarDropdown icon={<LineWeight />} />
+              <LinkToolbarButton icon={<Link />} />
+              <ImageToolbarButton icon={<Image />} />
+              <MediaEmbedToolbarButton icon={<OndemandVideo />} />
+              <TableToolbarButtons />
+            </HeadingToolbar>
+          </Toolbar>
+        </AppBar>
+        <Toolbar id="back-to-top-anchor" />
+        <MarkBallonToolbar />
 
-      <MarkBallonToolbar />
-
-      <MentionCombobox items={CONFIG.mentionItems} />
-    </Plate>
+        <MentionCombobox items={CONFIG.mentionItems} />
+      </Plate>
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </div>
   );
-};
-
-export default RichTextEditor;
+}
